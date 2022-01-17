eb33bc0567b174e5d650a38af45e5da86c2b3816
APURV CHOUBEY <apurv.main2@gmail.com>
2022-01-17 22:00:21 +0530

    Added categories

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ export class NavBar extends Component {
         return (
             <div>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <a className="navbar-brand" href="/">Navbar</a>
+                    <a className="navbar-brand" href="/">Categories</a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -17,8 +17,29 @@ export class NavBar extends Component {
                             <li className="nav-item active">
                                 <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
                             </li>
-                            <li className="nav-item">
+                            <li className="nav-item active">
                                 <a className="nav-link" href="/">About</a>
+                            </li>
+                            <li className="nav-item active">
+                                <a className="nav-link" href="/">Business</a>
+                            </li>
+                            <li className="nav-item active">
+                                <a className="nav-link" href="/">Entertainment</a>
+                            </li>
+                            <li className="nav-item active">
+                                <a className="nav-link" href="/">General</a>
+                            </li>
+                            <li className="nav-item active">
+                                <a className="nav-link" href="/">Health</a>
+                            </li>
+                            <li className="nav-item active">
+                                <a className="nav-link" href="/">Science</a>
+                            </li>
+                            <li className="nav-item active">
+                                <a className="nav-link" href="/">Sports</a>
+                            </li>
+                            <li className="nav-item active">
+                                <a className="nav-link" href="/">Technology</a>
                             </li>
                         </ul>
                     </div>
--- src/components/News.js
+++ src/components/News.js
@@ -1,87 +1,138 @@
-import React, { Component } from 'react';
-import NewsItem from './NewsItem';
-import Spinner from './Spinner';
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+import PropTypes from 'prop-types';
+
 
 export class News extends Component {
 
-    constructor(){
-        super();
-        this.state = {
-            articles : [],
-            loading : false,
-            page : 1
-        }
+    static defaultProps = {
+        country : 'in',
+        pageSize : 9,
+        category : 'general',
+    }
+    static propTypes = {
+        country : PropTypes.string,
+        pageSize : PropTypes.number,
+        category : PropTypes.string,
     }
 
-    async componentDidMount(){
-        
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=c6bcdcd9b1924d71a152a54cc34b1aa8&page=1&pageSize=${this.props.pageSize}`;
-        this.setState({loading : true});
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        this.setState({articles: parsedData.articles, 
-                       totalResults: parsedData.totalResults,
-                       loading : false});
-    }
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+    };
+  }
 
-    handlePrevClick = async ()=> {
-        
-        this.setState({loading : true});
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=c6bcdcd9b1924d71a152a54cc34b1aa8&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
-        let data = await fetch(url);
-        let parsedData = await data.json();
+  async componentDidMount() {
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c6bcdcd9b1924d71a152a54cc34b1aa8&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+      loading: false,
+    });
+  }
 
-        this.setState({
-            page: this.state.page-1,
-            articles: parsedData.articles,
-            loading : false
-        })
-    }
+  handlePrevClick = async () => {
+    this.setState({ loading: true });
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c6bcdcd9b1924d71a152a54cc34b1aa8&page=${
+      this.state.page - 1
+    }&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
 
-    handleNextClick = async ()=> {
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+      loading: false,
+    });
+  };
 
-        this.setState({loading : true});
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=c6bcdcd9b1924d71a152a54cc34b1aa8&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
-        let data = await fetch(url);
-        let parsedData = await data.json();
+  handleNextClick = async () => {
+    this.setState({ loading: true });
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c6bcdcd9b1924d71a152a54cc34b1aa8&page=${
+      this.state.page + 1
+    }&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
 
-        this.setState({
-            page : this.state.page+1,
-            articles : parsedData.articles,
-            loading : false
+    this.setState({
+      page: this.state.page + 1,
+      articles: parsedData.articles,
+      loading: false,
+    });
+  };
 
-        })
-        
-    }
+  render() {
+    return (
+      <>
+        <div className="justify-content-center">
+          <div className="container my-3 d-flex justify-content-center ">
+            <h2
+              className="font-weight-bold display-4 text-light"
+              style={{ margin: "35px 0px" }}
+            >
+              Today's Headlines
+            </h2>
+          </div>
 
-    render() {
-        return (
-            <>
-            <div className="container my-3 d-flex justify-content-center ">
-                    <h2 className="font-weight-bold display-4 text-light" >Today's Headlines</h2>
+          {this.state.loading && <Spinner />}
+
+          <div className="row">
+            {!this.state.loading &&
+              this.state.articles.map((element) => {
+                return (
+                  <div
+                    className="col-md-4 d-flex justify-content-center"
+                    key={element.url}
+                  >
+                    <NewsItem
+                      title={element.title != null ? element.title : " "}
+                      description={
+                        element.description != null
+                          ? element.description
+                          : " (Click Below to read this news) "
+                      }
+                      imageUrl={element.urlToImage}
+                      newsUrl={element.url}
+                    />
+                  </div>
+                );
+              })}
+
+            <div className="container d-flex justify-content-between">
+              <button
+                type="button"
+                className={`my-3 btn btn-danger ${
+                  this.state.page <= 1 ? "invisible" : "visible"
+                }`}
+                onClick={this.handlePrevClick}
+              >
+                &larr; Previous
+              </button>
+              <button
+                type="button"
+                className={`my-3 btn btn-danger ${
+                  this.state.page + 1 >
+                  Math.ceil(this.state.totalResults / this.props.pageSize)
+                    ? "invisible"
+                    : "visible"
+                }`}
+                onClick={this.handleNextClick}
+              >
+                Next &rarr;
+              </button>
             </div>
-
-            {this.state.loading && <Spinner />}
-
-            <div className="row" >
-                
-                {!this.state.loading && this.state.articles.map((element)=>{
-                    return <div className="col-md-4 d-flex justify-content-center" key={element.url}>
-                    <NewsItem title={element.title!=null?element.title:" "} description={element.description!=null?element.description:" "} imageUrl={element.urlToImage} newsUrl={element.url}/>
-                </div>
-                })}
-                
-                <div className="container d-flex justify-content-between">
-                    
-                    <button type="button" className={`my-3 btn btn-danger ${this.state.page<=1?'invisible':'visible'}`} onClick={this.handlePrevClick}>&larr; Previous</button>
-                    <button type="button" className={`my-3 btn btn-danger ${(this.state.page+1)> (Math.ceil(this.state.totalResults/this.props.pageSize))?'invisible':'visible'}`} onClick={this.handleNextClick}>Next &rarr;</button>
-                </div>
-
-                
-            </div>
-            </>
-        )
-    }
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
-export default News
+export default News;
