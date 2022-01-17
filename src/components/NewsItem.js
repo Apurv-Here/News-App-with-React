2dcc0110f5f0597d79b7873ad94697cd2d3d1b95
APURV CHOUBEY <apurv.main2@gmail.com>
2022-01-17 16:36:20 +0530

    Added news dynamically

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,15 +8,13 @@ export class NewsItem extends Component {
             <div className="my-3">
 
                 <div className="card" style={{width: "18rem"}}>
-                    <img className="card-img-top" src={imageUrl} alt=""/>
+                    <img className="card-img-top" src={imageUrl==null?"https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg":imageUrl} alt=""/>
                         <div className="card-body">
                             <h5 className="card-title"><strong>{title}</strong></h5>
                             <p className="card-text">{description}...</p>
-                            <a href={newsUrl} target="_blank" className="btn btn-primary btn-sm">Read more</a>
+                            <a href={newsUrl} target="_blank"  rel="noreferrer" className="btn btn-primary btn-sm">Read more</a>
                         </div>
                 </div>
-
-                
 
             </div>
         )
